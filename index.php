3391669dd345a6583fd65b6fed46ef1c976008f1
vnea <victtoor@hotmail.fr>
2016-10-14 12:59:22 +0200

    Start of home page skeleton

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Accueil</title>
+    </head>
+    
+    <body>
+        <header>
+          <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="signin.php">Connexion</a></li>
+            <li><a href="signup.php">Inscription</a></li>
+          </ul>
+        </header>
+        
+        <p>
+        La plateforme RobHub vous permet de partager ou de télécharger des comportements de Robobo. 
+        </p>
+    </body>
+</html>
