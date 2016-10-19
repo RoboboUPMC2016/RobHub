4ea9b2abab08a96165c66f9a7e0386188a8b39e6
vnea <victtoor@hotmail.fr>
2016-10-19 12:39:09 +0200

    new design

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+<footer id="fh5co-footer" role="contentinfo">
+  
+    <div class="container">
+      <div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>A propos de nous</h3>
+        <p>Nous sommes l'équipe Robobo.</p>
+      </div>
+
+<!--      <div class="col-md-6 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>Our Services</h3>
+        <ul class="float">
+          <li><a href="#">Web Design</a></li>
+          <li><a href="#">Branding &amp; Identity</a></li>
+          <li><a href="#">Free HTML5</a></li>
+          <li><a href="#">HandCrafted Templates</a></li>
+        </ul>
+        <ul class="float">
+          <li><a href="#">Free Bootstrap Template</a></li>
+          <li><a href="#">Free HTML5 Template</a></li>
+          <li><a href="#">Free HTML5 &amp; CSS3 Template</a></li>
+          <li><a href="#">HandCrafted Templates</a></li>
+        </ul>
+
+      </div>
+
+      <div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>Follow Us</h3>
+        <ul class="fh5co-social">
+          <li><a href="#"><i class="icon-twitter"></i> Twitter</a></li>
+          <li><a href="#"><i class="icon-facebook"></i> Facebook</a></li>
+          <li><a href="#"><i class="icon-google-plus"></i> Google Plus</a></li>
+          <li><a href="#"><i class="icon-instagram"></i> Instagram</a></li>
+        </ul>
+      </div> -->
+      
+      
+      <div class="col-md-12 fh5co-copyright text-center">
+        <p>&copy; UPMC GPSTL 2016</p> 
+      </div>
+      
+    </div>
+</footer>
--- signin.php
+++ signin.php
@@ -7,7 +7,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Accueil</title>
+	<title>Connexion</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- 
@@ -60,68 +60,45 @@
 				<h1><img src="images/robhub.svg" alt="robhub logo"><a href="index.php">RobHub</a></h1>
 				<nav role="navigation">
 					<ul>
-						<li><a class="active" href="index.php">Accueil</a></li>
+						<li><a href="index.php">Accueil</a></li>
 						<li><a href="signup.php">Inscription</a></li>
-						<li><a href="signin.php">Connexion</a></li>
+						<li><a class="active" href="signin.php">Connexion</a></li>
 					</ul>
 				</nav>
 			</div>
 		</div>
 	</header>
-	<div id="fh5co-intro-section">
+	<div id="fh5co-contact-section">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-8 col-md-offset-2 text-center">
-					<h2>Bienvenue sur le réseau RobHub</h2>
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Connexion</h2>
 				</div>
 			</div>
-            
-      <div class="row">
-				<div class="col-md-4 text-center">
-					<div class="blog-inner">
-						<a href="#"><img class="img-responsive" src="images/image_4.jpg" alt="Blog"></a>
-						<div class="desc">
-							<h3><a href="https://github.com/RoboboUPMC2016/RobApp">RobApp</a></h3>
-							<p> 
-							RobApp est l'application permettant de donner un comportement à votre robobo.
-              Avec RobApp vous donnez au robobo les comportements que vous créé avec RobDev.
-              Vous pouvez aussi télécharger les comportement partager sur RobHub directement
-              depuis votre smartphone.
-              </p>
+			<div class="row">
+				<div class="col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+					<form method="post" class="row">
+						<div class="col-md-11">
+							<div class="form-group">
+								<input class="form-control" name="login" placeholder="Identifiant" type="text">
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-md-4 text-center">
-					<div class="blog-inner">
-						<a href="#"><img class="img-responsive" src="images/image_5.jpg" alt="Blog"></a>
-						<div class="desc">
-							<h3><a href="https://github.com/RoboboUPMC2016/RobHub">RobHub</a></h3>
-							<p>
-							RobHub est le reseau social de votre robobo. Vous pouvez partager des comportements
-              créés avec le framework RobDev, mais aussi des vidéos et des photos.
-              </p>
+						<div class="col-md-11">
+							<div class="form-group">
+								<input class="form-control" name="password" placeholder="Mot de passe" type="password">
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-md-4 text-center">
-					<div class="blog-inner">
-						<a href="#"><img class="img-responsive" src="images/image_6.jpg" alt="Blog"></a>
-						<div class="desc">
-							<h3><a href="https://github.com/RoboboUPMC2016/RobDev">RobDev</a></h3>
-							<p> 
-							RobDev est un framework pour créer les comportements de votre Robobo. Pour savoir comment
-              créer un comportement rendez vous sur le
-              <a href="https://github.com/RoboboUPMC2016/RobDev/wiki">Wiki du projet RobDev</a>.
-              </p>
+						<div class="col-md-12">
+							<div class="form-group">
+								<input value="Se connecter" class="btn btn-primary" type="submit">
+							</div>
 						</div>
-					</div>
+					</form>
+
 				</div>
 			</div>
 		</div>
-        
-        
 	</div>
-				
 	
 	<?php include "includes/footer.php" ?>
 
@@ -136,6 +113,9 @@
 	<script src="js/bootstrap.min.js"></script>
 	<!-- Waypoints -->
 	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
 	<!-- MAIN JS -->
 	<script src="js/main.js"></script>
 
